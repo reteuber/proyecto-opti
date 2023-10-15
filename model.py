f3ddef41db7d65a71857025b29086a942f743167
Re <rsteuber@uc.cl>
2023-10-15 11:46:59 -0300

    archivo gurobi

---
+++ model.py
@@ -0,0 +1,20 @@
+from gurobipy import GRB, Model, quicksum
+import random
+
+# defininir los conjuntos
+
+# definir los parámetros
+
+# crear el modelo vacío (model = Model())
+
+# definir las variables
+
+# llamar al update (model.update())
+
+# definir las restricciones
+
+# hacer el setObjective (GRB.MINIMIZE)
+
+# model.optimize()
+
+# printear soluciones
